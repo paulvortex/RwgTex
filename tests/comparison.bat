9c12f379494511732fb17e3940f9ab007ee1df62
Pavel Timofeyev <paul.vortex@gmail.com>
2016-12-06 00:15:54 +0300

    Fixes

--- tests/comparison.bat
+++ tests/comparison.bat
@@ -13,27 +13,27 @@ FOR %%i IN (images\*.*) DO (
 	echo %%~ni
 	REM --- DXT ---
 	FOR %%t IN (nv ati gimp crunch pvrtex) DO (
-		echo ..%%t ^(DXT^)
+		echo ..%%t ^(DXT^) : -dxt -%%t
 		%rwgtex% -f "%%i" -dxt -o "~comparison_%profile%\%%~ni" -%%t -%profile% -t
 		FOR %%x IN (rxgb ycg1 ycg2) DO (
-			echo ..%%t ^(DXT,%%x^)
+			echo ..%%t ^(DXT,%%x^) : -dxt -%%t -%%x
 			%rwgtex% -f "%%i" -dxt -o "~comparison_%profile%\%%~ni" -%%t -%profile% -t -%%x
 		)
 	)
 	REM --- ETC1 ---
 	FOR %%t IN (etcpack ati rgetc1 pvrtex) DO (
-		echo ..%%t ^(ETC1^)
+		echo ..%%t ^(ETC1^) : -etc1 -%%t
 		%rwgtex% -f "%%i" -etc1 -o "~comparison_%profile%\%%~ni" -%%t -%profile% -t
 	)
 	REM --- ETC2 ---
 	FOR %%t IN (etcpack, etc2comp pvrtex) DO (
-		echo ..%%t ^(ETC2^)
+		echo ..%%t ^(ETC2^) : -etc2 -%%t
 		%rwgtex% -f "%%i" -etc2 -o "~comparison_%profile%\%%~ni" -%%t -%profile% -t
 	)
 	REM --- PVRTC ---
 	FOR %%t IN (pvrtex) DO (
 		FOR %%x IN (pvr2 pvr4) DO (
-			echo ..%%t ^(PVRTC,%%x^)
+			echo ..%%t ^(PVRTC,%%x^) : -pvrtc -%%t -%%x
 			%rwgtex% -f "%%i" -pvrtc -o "~comparison_%profile%\%%~ni" -%%t -%profile% -tk -%%x
 		)
 	)
--- tests/make_archives.bat
+++ tests/make_archives.bat
@@ -3,7 +3,7 @@ rmdir ~archives /S /Q
 mkdir ~archives
 set rwgtex=..\win32\rwgtex.exe
 echo --- packing test.pk3 ---
-%rwgtex% "images" -dxt -etc -pvr -o "~archives/test.pk3"
+%rwgtex% "images" -dxt -etc -pvr -o "~archives/test.pk3" -ap "textures/"
 echo --- packing test.pk3 (zipmem) ---
-%rwgtex% "images" -dxt -etc -pvr -o "~archives/test_zipmem.pk3" -zipmem 50
+%rwgtex% "images" -dxt -etc -pvr -o "~archives/test_zipmem.pk3" -ap "textures/" -zipmem 50
 pause
